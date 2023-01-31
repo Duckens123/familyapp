cb79b696f20af3eec21d5ab6575ea50632b78d28
Duckenson jacquet <duckens244@hotmail.com>
2023-01-31 11:18:27 -0500

    Correct Build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #Build the project
 
 echo "Building the project"
-python3.10.9 -m pip install requirements.txt
+python3 -m pip install requirements.txt
 
 echo "Make migration...."
-python3.10.9 manage.py makemigrations --noinput
-python3.10.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect statistics..."
 
